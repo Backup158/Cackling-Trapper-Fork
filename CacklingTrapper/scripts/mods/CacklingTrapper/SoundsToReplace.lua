6467b73eb546abd3bf773d9b3e9147d4c60e532a
Backup158 <beekoop158@gmail.com>
2025-12-31 23:03:48 -0600

    feat: hounds and mauler

--- CacklingTrapper/scripts/mods/CacklingTrapper/SoundsToReplace.lua
+++ CacklingTrapper/scripts/mods/CacklingTrapper/SoundsToReplace.lua
@@ -1,8 +1,20 @@
 local mod = get_mod("CacklingTrapper")
 
 mod.table_of_replacements = {
-    ["trapper"] = {
+    ["renegade_netgunner"] = {
         ["breed_file_path"] =   "scripts/settings/breed/breeds/renegade/renegade_netgunner_sounds",
         ["wwise_event_path"] =  "wwise/events/minions/play_traitor_guard_netgunner_laugh_vce",
-    }
+    },
+    ["renegade_executor"] = {
+        ["breed_file_path"] =   "scripts/settings/breed/breeds/renegade/renegade_executor_sounds",
+        ["wwise_event_path"] =  "wwise/events/minions/play_enemy_traitor_executor__special_attack_vce",
+    },
+    ["chaos_hound"] = {
+        ["breed_file_path"] =   "scripts/settings/breed/breeds/chaos/chaos_hound_sounds",
+        ["wwise_event_path"] =  "wwise/events/minions/play_enemy_chaos_hound_vce_bark",
+    },
+    ["chaos_hound_mutator"] = {
+        ["breed_file_path"] =   "scripts/settings/breed/breeds/chaos/chaos_hound_mutator_sounds",
+        ["wwise_event_path"] =  "wwise/events/minions/play_enemy_chaos_hound_vce_bark",
+    },
 }
